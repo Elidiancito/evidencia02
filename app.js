6adc365280e38a010d3829ca9cc1f67d582d085b
Elidiancito <100305627+Elidiancito@users.noreply.github.com>
2022-03-07 20:48:58 -0700

    evidencia02

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require ("http");
+const express = require("express");
+const app = express();
+
+// Codificar request 
+
+app.get ('/',(req,res)=> {
+
+    res.send("Iniciamos servidor");
+})
+
+const puerto = 3000;
+app.listen(puerto,()=>{
+    console.log(" Escuchando");
+});
